cfd32a7990abe8adb0a3f69954cd1387a79989fd
anisole <anisolepower0235@gmail.com>
2023-02-16 22:08:22 +0530

    Initial commit

--- routes/LoginRoute.js
+++ routes/LoginRoute.js
@@ -4,6 +4,8 @@ const userSchema = require('../models/UserSchema');
 
 
 router.post("/", async (req, res) => {
+
+    console.log(req.body);
     let result = await userSchema.findOne({ "username": req.body.username, "password": req.body.password });
     if (result == null) { return res.json({ "error": "Invalid Username or Password" }) }
 
