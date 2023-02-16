162394cc7fff076ba145cac46d861d55083a1a2b
anisole <anisolepower0235@gmail.com>
2023-02-16 21:22:09 +0530

    fix bug

--- routes/LoginRoute.js
+++ routes/LoginRoute.js
@@ -4,8 +4,8 @@ const userSchema = require('../models/UserSchema');
 
 
 router.post("/", async (req, res) => {
-    let result = await userSchema.findOne(req.body);
-    if (result.length == 0) { return res.json({ "error": "User not Exists" }) }
+    let result = await userSchema.findOne({ "username": req.body.username });
+    if (result) { return res.json({ "error": "User not Exists" }) }
 
 
     if (req.body.username == result.username && req.body.password == result.password) {
