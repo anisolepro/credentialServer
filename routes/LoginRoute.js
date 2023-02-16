f173829a2f5370d365fe802d5679bd79d5a55743
anisole <anisolepower0235@gmail.com>
2023-02-16 22:27:20 +0530

    add cors

--- routes/LoginRoute.js
+++ routes/LoginRoute.js
@@ -1,14 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router()
 const userSchema = require('../models/UserSchema');
 
-router.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+app.use(cors({
+    origin: '*'
+}));
 router.post("/", async (req, res) => {
 
     console.log(req.body);
