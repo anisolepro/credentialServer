dc9880936e4d828be736b86b6abd2a7de4d75dc1
anisole <anisolepower0235@gmail.com>
2023-02-16 21:15:38 +0530

    added cors

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ mongoose.connect(uri, () => { console.log("connected") });
 
 // express endpoints
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send("anisole");
 })
