d9fcfdf0fea5977cfefe5a37e2c934de69d129cc
anisole <anisolepower0235@gmail.com>
2023-02-19 10:47:27 +0530

    added profile Pic Upload feature du

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -17,6 +17,10 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    profilePicType: {
+        type: String,
+        required: true
+    },
     verification: {
         type: Boolean,
         default: false
--- routes/SignUpRoute.js
+++ routes/SignUpRoute.js
@@ -3,31 +3,68 @@ const router = express.Router()
 const userSchema = require('../models/UserSchema');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const cors = require('cors');
+const fs = require('fs');
 
+router.use(cors());
+
+
+let tempFileName;
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './profilePics')
+    },
+    filename: function (req, file, cb) {
+        tempFileName = `${Date.now()}-${file.originalname}`;
+        cb(null, tempFileName)
+    }
+})
+
+const upload = multer({ storage: storage }).single("profilePic")
 
 router.post("/", async (req, res) => {
+
+
+    await upload(req, res, function (err) {
+        if (err) {
+            return res.end("Error uploading file.");
+        }
+    });
+
+
+
+
     let user = await userSchema.find({ "username": req.body.username });
     if (user.length > 0) { return res.json({ "error": "username already exists" }) }
     user = await userSchema.find({ "email": req.body.email });
     if (user.length > 0) { return res.json({ "error": "Email already exists" }) }
 
     try {
-        let hash = await bcrypt.hash(req.body.password, 10);
 
-        // console.log(hash);
+
+
+
+        let hash = await bcrypt.hash(req.body.password, 10);
+        // return res.send("uploaded")
 
 
         user = await userSchema({
             "username": req.body.username,
             "email": req.body.email,
-            "password": hash
+            "password": hash,
+            "profilePicType": `${tempFileName.split(".").at(-1)}`
         });
-        user.save();
+        await user.save();
+
+
+
+
+        fs.rename(`profilePics/${tempFileName}`, `profilePics/${user.id}.${user.profilePicType}`, () => { })
         let token = jwt.sign({ "auth": user.id }, 'secretKey69');
 
+        // console.log(req.body)
 
-        // console.log(result.id)
-        // console.log(jwt.verify(token, "secretKey69").auth);
         let message = " Successfully Signed Up"
         res.send({ token, message });
     }
@@ -36,4 +73,4 @@ router.post("/", async (req, res) => {
     }
 })
 
-module.exports = router
+module.exports = router;
